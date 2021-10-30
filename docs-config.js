f03666ccb24879c86ff6a481a1e365e88e96e7d2
whomihirpatel <mspatel1298@gmail.com>
2021-10-30 06:31:14 -0500

    initiated package and docs-config using cloudflare docs engine tempelate

---
+++ docs-config.js
@@ -0,0 +1,29 @@
+module.exports = {
+    product: "Pegasus Docs",
+    pathPrefix: "",
+    productIconKey: "",
+    contentRepo: "PegasusDrone/docs",
+    contentRepoFolder: "",
+    externalLinks: [
+      {
+        title: "Pegasus Docs",
+        url: "https://mihr.io/docs"
+      },
+      {
+        title: "Pegasus Drone documentation",
+        url: "https://mihr.io/docs"
+      },
+    ],
+    search: {
+      indexName: "",
+      apiKey: "",
+      algoliaOptions: { "facetFilters": "" }
+    },
+    siteMetadata: {
+      title: "Pegasus Docs",
+      description: "Documentation for the Northwestern University CRB's Pegasus Drone",
+      author: "@mihyr",
+      url: "http://mihr.io",
+      image: "https://github.com/mihyr.png",
+    }
+  }
